cf90c94ef48a9d976f4e0af4332d85313f8eb097
Julia Schatzmann <julia.schatzmann@students.fhv.at>
2017-11-20 12:01:16 +0100

    wip

--- src/bean/Display.java
+++ src/bean/Display.java
@@ -1,18 +1,22 @@
 package bean;
 
+import com.sun.media.jai.widget.DisplayJAI;
+
 import javax.media.jai.PlanarImage;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
+import java.io.Serializable;
 import java.util.Vector;
 
-public class Display extends Canvas implements IFilterListener {
-    private PlanarImage image;
+public class Display extends Canvas implements Serializable, IFilterListener {
+
     private Vector listeners;
+    private PlanarImage image;
 
     public Display() {
-        listeners = new Vector();
-        image = null;
+        this.listeners = new Vector();
     }
 
     public PlanarImage getImage() {
@@ -22,6 +26,7 @@ public class Display extends Canvas implements IFilterListener {
     public void setImage(PlanarImage image) {
         this.image = image;
     }
+
     public void addIFilterListener(IFilterListener filterListener) {
         listeners.addElement(filterListener);
     }
@@ -37,7 +42,24 @@ public class Display extends Canvas implements IFilterListener {
     }
 
     public void paint(Graphics g) {
+        // Create a frame for display.
+        JFrame frame = new JFrame();
+        frame.setTitle("DisplayJAI: loetstellen.jpg");
 
+        // Get the JFrame ContentPane.
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        // Create an instance of DisplayJAI.
+        DisplayJAI dj = new DisplayJAI(this.image);
+
+        // Add to the JFrame ContentPane an instance of JScrollPane
+        // containing the DisplayJAI instance.
+        contentPane.add(new JScrollPane(dj),BorderLayout.CENTER);
+
+        // Set the closing operation so the application is finished.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500); // adjust the frame size.
+        frame.setVisible(true); // show the frame.
     }
-
 }
